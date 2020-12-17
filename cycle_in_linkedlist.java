3a40326873ddcfac3e927499514bbf1128ab6424
Akshay Hiremath <49857679+akahay159@users.noreply.github.com>
2020-12-17 22:47:08 +0530

    Add files via upload

---
+++ cycle_in_linkedlist.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast)
+                return true;
+            //else return false;
+        }
+        return false;
+    }
+}
+
+
+
+// time complexity - O(n) to traverse the linked list
+// space complexity - O(1) constant space is used
